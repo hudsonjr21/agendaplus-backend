8e32846a2db88ef59bfd24b21d4c563c18a9b7b2
Hudson Ferreira <hudsonferreiramb@hotmail.com>
2025-03-13 13:00:51 -0300

    tables ok

---
+++ data-source.ts
@@ -0,0 +1,17 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); // Carrega as variáveis de ambiente do .env
+import { DataSource } from 'typeorm';
+import { ENTITIES } from './src/infra/database/entities';
+
+export const AppDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION as 'postgres', // Defina 'postgres' diretamente, pois é o valor no .env
+  host: process.env.TYPEORM_HOST,
+  port: Number(process.env.TYPEORM_PORT),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: ENTITIES,
+  migrations: [process.env.TYPEORM_MIGRATIONS || 'dist/shared/migrations/*.js'],
+  synchronize: process.env.TYPEORM_SYNCHRONIZE === 'false',
+  logging: true,
+});
